d9e3108e617397b30a57f7173533983f936c4f2f
chiangyiyang <chiangyiyang@gmail.com>
2024-03-20 15:18:28 +0800

    20240320

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 from flask import Flask, request
-from markupsafe import escape
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello Flask! </p>"
-
 db = []
-
-@app.route("/t/<t>")
-def getTemp(t):
-    return f"Temperature: {escape(t)}!"
-
-@app.route("/h/<h>")
-def getHumidity(h):
-    return f"Humidity: {escape(h)}!"
 
 @app.route("/th")
 def getTemperatureHumidity():
@@ -24,7 +10,6 @@ def getTemperatureHumidity():
     db += [[args.get("t"), args.get("h")]]
     return db
 
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
